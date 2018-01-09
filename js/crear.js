1cff82565956ff675e71c4482d734805f6dff12f
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-09 12:59:59 -0500

    implementacion de la opcion crear

--- js/crear.js
+++ js/crear.js
@@ -1,4 +1,12 @@
 /* EMAIL */
-function signIn() {
-  console.log('diste un clic');
+function crear() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 }
