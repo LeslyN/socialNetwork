1c940e0ef5ec60daf106c14ddf4d0eebafa6239a
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-12 01:41:49 -0500

    facebook inicia sesion - 2do intento

--- js/login-red-social.js
+++ js/login-red-social.js
@@ -11,9 +11,11 @@ firebase.initializeApp(config);
 
 
 /* ******************************GOOGLE**************************** */
-var provider = new firebase.auth.GoogleAuthProvider();
+
 
 function google() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
   firebase.auth().signInWithPopup(provider)
     .then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
@@ -40,28 +42,26 @@ function google() {
 
 
 function facebook() {
-  if (!firebase.auth().currentUser) {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('public_profile');
+  var provider = new firebase.auth.FacebookAuthProvider();
+  provider.addScope('public_profile');
 
-    firebase.auth().signInWithPopup(provider)
-      .then(function(result) {
+  firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log('usuario activo');
-        window.location.href = '../views/profile.html';
-      })
-      .catch(function(error) {
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log('usuario activo');
+      window.location.href = '../views/profile.html';
+    })
+    .catch(function(error) {
       // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
       // ...
-      });
-  }
+    });
 }
