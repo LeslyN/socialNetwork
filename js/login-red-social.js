48dabe3db841835e11aef843bc59e9ac822370f9
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-12 01:32:48 -0500

    js de facebook conectado a profile (1er intento)

--- js/login-red-social.js
+++ js/login-red-social.js
@@ -13,7 +13,6 @@ firebase.initializeApp(config);
 /* ******************************GOOGLE**************************** */
 var provider = new firebase.auth.GoogleAuthProvider();
 
-
 function google() {
   firebase.auth().signInWithPopup(provider)
     .then(function(result) {
@@ -35,4 +34,34 @@ function google() {
       var credential = error.credential;
     // ...
     });
+}
+
+/* ******************************FACEBOOK**************************** */
+
+
+function facebook() {
+  if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('public_profile');
+
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log('usuario activo');
+        window.location.href = '../views/profile.html';
+      })
+      .catch(function(error) {
+      // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+      // ...
+      });
+  }
 }
