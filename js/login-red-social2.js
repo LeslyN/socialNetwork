5cfc913fdf79200f48bdab4bab1e4d22e99a97cd
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-12 08:17:59 -0500

    prueba google 1

--- js/login-red-social2.js
+++ js/login-red-social2.js
@@ -9,41 +9,12 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
-/* ******************************GOOGLE**************************** */
-
-
-function google() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-
-  firebase.auth().signInWithPopup(provider)
-    .then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      console.log('usuario activo');
-      window.location.href = '../views/profile.html';
-    // ...
-    })
-    .catch(function(error) {
-    // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-    // ...
-    });
-}
-
 /* ******************************FACEBOOK**************************** */
-
+var provider = new firebase.auth.FacebookAuthProvider();
 
 function facebook() {
   if (!firebase.auth().currentUser) {
-    var provider = new firebase.auth.FacebookAuthProvider();
+    
 
     provider.addScope('public_profile');
 
