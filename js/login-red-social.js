0c37bf3e18425c8f2a0888f3a292fea53becb0b2
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-12 02:20:26 -0500

    facebook 4to intento

--- js/login-red-social.js
+++ js/login-red-social.js
@@ -53,6 +53,7 @@ function facebook() {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
+        var name = result.user.displayName;
         console.log('usuario activo');
         window.location.href = '../views/profile.html';
       })
