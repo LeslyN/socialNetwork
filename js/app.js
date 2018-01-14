76ab4ed477efbc88e30918f9288b97d664e1b4b2
LeslyN <lnombertoc@gmail.com>
2018-01-13 22:35:57 -0500

    reestructurando

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* Vista splash de index a sesion en 5 segundos */
 $(document).ready(function() {
   setTimeout(function() { 
-    window.location.href = 'views/sesion.html';
+    window.location.href = 'views/create.html';
   }, 5000);
 });
