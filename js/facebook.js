6deb34d4b5e4167fd484fafa7cf74721230ce6ee
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-14 16:18:35 -0500

    funcionalidad con facebook prueba 4

--- js/facebook.js
+++ js/facebook.js
@@ -11,27 +11,25 @@ firebase.initializeApp(config);
 
   
 /* ******************************FACEBOOK**************************** */
+var user = null;
 var provider = new firebase.auth.FacebookAuthProvider();
   
 function facebook() {
-  firebase.auth().signInWithRedirect(provider)
-    .then(function(result) {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      console.log('usuario activo');
-      /* Direcciona a la vista profile */
-      window.location.href = '../views/profile.html';
-    }).catch(function(error) {
-    // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
     // ...
-    });
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 }
 
