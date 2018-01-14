5741c44013bd62101181a2624b141d0a663ca7f8
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-01-14 11:39:33 -0500

    funcionalidad facebook (prueba 3)

--- js/facebook.js
+++ js/facebook.js
@@ -14,28 +14,24 @@ firebase.initializeApp(config);
 var provider = new firebase.auth.FacebookAuthProvider();
   
 function facebook() {
-  firebase.auth().signInWithRedirect(provider);
-
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  firebase.auth().signInWithRedirect(provider)
+    .then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       var token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-    console.log('usuario activo');
-    /* Direcciona a la vista profile */
-    window.location.href = '../views/profile.html';
-  }).catch(function(error) {
+      // The signed-in user info.
+      var user = result.user;
+      console.log('usuario activo');
+      /* Direcciona a la vista profile */
+      window.location.href = '../views/profile.html';
+    }).catch(function(error) {
     // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
     // ...
-  });
+    });
 }
 
